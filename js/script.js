d90bff96827a02ac6c942163f53026c0f63f22f9
Omar-Codess <omartaki8@gmail.com>
2022-10-05 16:58:55 +0200

    creata variabile che chiede il cognome

--- js/script.js
+++ js/script.js
@@ -2,4 +2,7 @@
 const userName = prompt("Qual è il tuo nome?");
 console.log(userName, typeof userName);
 
+// Cognome utente
+const userSurname = prompt("Qual è il tuo cognome?");
+console.log(userSurname, typeof userSurname);
 
