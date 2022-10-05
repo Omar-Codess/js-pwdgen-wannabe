8729abc763c7d3298d1fd0737597db7c61c800f5
Omar-Codess <omartaki8@gmail.com>
2022-10-05 17:20:39 +0200

    inserita variabile risultato

--- js/script.js
+++ js/script.js
@@ -1,11 +1,23 @@
-// Nome utente
+// INPUT
+// Chiedere nome utente
 const userName = prompt("Qual è il tuo nome?");
 console.log(userName, typeof userName);
 
-// Cognome utente
+// Chiedere cognome utente
 const userSurname = prompt("Qual è il tuo cognome?");
 console.log(userSurname, typeof userSurname);
 
-// Colore preferito utente
+// Chiedere colore preferito utente
 const favColor = prompt("Qual è il tuo colore preferito?")
 console.log(favColor, typeof favColor);
+
+// ELABORAZIONE
+const password = userName + userSurname + favColor + 21;
+console.log(password);
+
+// OUTPUT
+// Frase output
+const result = `La tua password è ${password}`;
+
+// Scrivere output
+document.getElementById("result").innerHTML = result;
